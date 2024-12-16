2da2c4eb97e828c99da833887ab4b99c18131e40
momeng00 <69623137+momeng00@users.noreply.github.com>
2024-12-17 06:08:01 +0900

    범위 공격 요구사항 해결
    타겟을 지정하고 그 타겟의 일정 범위에 있는 몬스터만을 타겟으로 지정함.
    + 임시용 fsm설정, lock기능

--- Assets/Scripts/Game/Unit/State/DeathState.cs
+++ Assets/Scripts/Game/Unit/State/DeathState.cs
@@ -6,6 +6,7 @@ public class DeathState : StateBase, IState
     public void OnEnter(Unit unit)
     {
         unit.CrossFade("Death", 0f);
+        _fsm.LockState();
     }
 
     public void OnExit(Unit unit)
--- Assets/Scripts/Game/Unit/State/SpecialDeathState.cs
+++ Assets/Scripts/Game/Unit/State/SpecialDeathState.cs
@@ -6,6 +6,7 @@ public class SpecialDeathState : StateBase, IState
     public void OnEnter(Unit unit)
     {
         unit.CrossFade("SpecialDeath", 0f);
+        _fsm.LockState();
     }
 
     public void OnUpdate(Unit unit)
