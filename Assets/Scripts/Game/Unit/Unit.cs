3ac75ecf57c39b2941f450a2165deb4ff4c7c312
MarkDPirate <mark@catze.xyz>
2024-12-19 18:09:28 +0900

    스테이지 병라합이슈해결중

--- Assets/Scripts/Game/Unit/Unit.cs
+++ Assets/Scripts/Game/Unit/Unit.cs
@@ -339,8 +339,8 @@ public class Unit : MonoBehaviour, IStats, IStatSetable, IStatUpdatable
 
         if (Health.Value <= 0)
         {
-            //OnDeath(attacker, ActiveSpecialDeath);
-            OnDeath(attacker, true);
+            OnDeath(attacker, ActiveSpecialDeath);
+            //OnDeath(attacker, true);
             //죽었다 하면 무조껀 스페셜 데스 하게 만드는 코드
         }
     }
