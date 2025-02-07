cc210644092b0116e4deeda6af5ab77c5d229542
momeng00 <69623137+momeng00@users.noreply.github.com>
2025-02-07 21:44:29 +0900

    스킬 버그 수정
    EnterOnce가 의도한대로 작동하지않아 삭제처리

--- Assets/Scripts/Data/Game/FxEventData/Skill/LightingSpearSkillFxEventData.cs
+++ Assets/Scripts/Data/Game/FxEventData/Skill/LightingSpearSkillFxEventData.cs
@@ -6,14 +6,19 @@ using static UnityEngine.UI.CanvasScaler;
     menuName = "Data/SkillData/FxEventData/LightingSpearSkillFxEventData")]
 public class LightingSpearSkillFxEventData : SkillFxEventData
 {
+    [SerializeField] private ProjectileData _lightingSpearDotPrefab;
     [SerializeField] private ProjectileData _lightingSpearPrefab;
 
     public override void OnSkillEvent(Unit owner, Skill skill)
     {
-        var spawnPrefab = ResourceManager.Instance.Spawn(_lightingSpearPrefab.Prefab.gameObject);
+        var spawnPrefab = ResourceManager.Instance.Spawn(_lightingSpearDotPrefab.Prefab.gameObject);
+        var spawnSpearPrefab = ResourceManager.Instance.Spawn(_lightingSpearPrefab.Prefab.gameObject);
         var projectile = spawnPrefab.GetComponent<Projectile>();
-        projectile.OnFire(owner, _lightingSpearPrefab);
+        var projectileSpear = spawnSpearPrefab.GetComponent<Projectile>();
+        projectile.OnFire(owner, _lightingSpearDotPrefab);
+        projectileSpear.OnFire(owner, _lightingSpearPrefab);
         spawnPrefab.transform.position = owner.Target.transform.position;
+        spawnSpearPrefab.transform.position = owner.Target.transform.position;
         //unit.OnHit(owner.Attack.Value /*마력으로*/, owner);
     }
 
